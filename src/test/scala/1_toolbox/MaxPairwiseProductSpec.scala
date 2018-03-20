ddde1ec47c22786f3b3f8a29974a35ae0dd1c32c
Pavel <pavel.oliynyk@gmail.com>
2018-03-20 21:12:44 +0000

    Add unit test

--- src/test/scala/1_toolbox/MaxPairwiseProductSpec.scala
+++ src/test/scala/1_toolbox/MaxPairwiseProductSpec.scala
@@ -15,4 +15,13 @@ class MaxPairwiseProductSpec extends FlatSpec with Matchers {
     assertResult( 6 )( pp.max2() )
   }
 
+  "Max for Seq(7 5 14 2 8 8 10 1 2 3)" should " be 140 " in {
+
+    val s = Seq(7, 5, 14, 2, 8, 8, 10, 1, 2, 3)
+
+    val pp = MaxPairwiseProduct( s.length, s)
+
+    assertResult( 140 )( pp.max2() )
+  }
+
 }
